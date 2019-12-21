160de036e3e93adee8c86fda5f247871d8957c8a
idaned <49078179+idaned@users.noreply.github.com>
2019-12-21 12:09:48 +0200

    Update hello-docker.sh

--- hello-docker.sh
+++ hello-docker.sh
@@ -7,18 +7,18 @@ while [[ `ping $1 -t 1 -c 1 | grep "+1 errors"` ]];do
 done
 
 if [ ! `ssh -o "StrictHostKeyChecking no" -i $2 ubuntu@$1 "which docker"` ]; then
-	wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb
-	wget http://mirrors.kernel.org/ubuntu/pool/main/libt/libtool/libltdl7_2.4.6-0.1_amd64.deb
-	sudo docker pull hello-world
-	sudo docker save -o hello-world.docker hello-world;
-	
-	scp -i $2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-     docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb \
-     libltdl7_2.4.6-0.1_amd64.deb \
-     hello-world.docker \
-     ubuntu@$1:/tmp/
-	
-	ssh -i $2 ubuntu@$1 "sudo dpkg -i /tmp/libltdl7_2.4.6-0.1_amd64.deb && 
+  wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb
+  wget http://mirrors.kernel.org/ubuntu/pool/main/libt/libtool/libltdl7_2.4.6-0.1_amd64.deb
+  sudo docker pull hello-world
+  sudo docker save -o hello-world.docker hello-world;
+  
+  scp -i $2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb \
+    libltdl7_2.4.6-0.1_amd64.deb \
+    hello-world.docker \
+    ubuntu@$1:/tmp/
+  	
+  ssh -i $2 ubuntu@$1 "sudo dpkg -i /tmp/libltdl7_2.4.6-0.1_amd64.deb && 
     sudo dpkg -i /tmp/docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb &&
     sudo docker load -i hello-world.docker"
 fi
